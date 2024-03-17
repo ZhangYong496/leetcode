5df75a1c4c67a8e6f73d683e45d5c1f8a3999628
Alex496 <Alex_Zhang99@163.com>
2024-03-17 23:02:39 +0800

    feat(全局异常定义): 全局异常定义
    全局异常定义

--- src/main/java/com/multiThread/demo/test02/Demo01.java
+++ src/main/java/com/multiThread/demo/test02/Demo01.java
@@ -3,5 +3,13 @@ package com.multiThread.demo.test02;
 public class Demo01 {
     public static void main(String[] args) {
 
+        SendGifts sendGifts = new SendGifts();
+
+        Thread t1 = new Thread(sendGifts, "同学A");
+        Thread t2 = new Thread(sendGifts, "同学B");
+        t1.start();
+        t2.start();
+
+
     }
 }
